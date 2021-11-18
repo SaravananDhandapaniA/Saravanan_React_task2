db7f10199ea806e2d4472301cc7ca886fd874ed4
SaravananDhandapaniA <91460255+SaravananDhandapaniA@users.noreply.github.com>
2021-11-18 11:33:38 +0530

    Update App.js

--- add-to-cart/src/App.js
+++ add-to-cart/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useEffect } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Cart from './Cart';
 import Home from "./Home";
@@ -9,7 +9,7 @@ import {CartProvider} from "react-use-cart";
 
 function App() {
 
-//  const [products,setProducts] = useState([]);
+
 
   useEffect(() => {
     fetchAPI()
@@ -19,7 +19,7 @@ function App() {
     return fetch('http://localhost:5000/api/rates')
     .then(response => response.json())
     .then(data => { 
-      //setProducts(data)
+     
       console.log(data)
     });
   }
